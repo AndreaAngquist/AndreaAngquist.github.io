0425ed65fc58003dd73773cd8670460d1927d478
Andrea Angquist <andreaangquist@gmail.com>
2014-12-15 21:14:43 -0800

    updated all js files

--- js/old_main.js
+++ js/old_main.js
@@ -284,7 +284,7 @@ jQuery(document).ready(function($) {
 		} else if($('body').hasClass('image-background')) {
 		
 			$("body").backstretch([
-				"http://placehold.it/1980x1080"
+				"images/coloredpencils_bw.jpg"
 			]);
 			
 		}
